0b442b4aef51c13851f7b3025a6087a480d149d8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-07-20 04:01:42 +0000

    Update manage_prolific.sh

--- install-scripts/4-server/files/manage_prolific.sh
+++ install-scripts/4-server/files/manage_prolific.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-{
-  chmod a+r /dev/ttyLYS_prolific_"$1"
-  if [[ $2 == "remove" ]] ; then
-    logger "The USB device /dev/ttyLYS_prolific_$1 has been disconnected"
-  else
-    logger "This USB device is known as Prolific and will be linked on /dev/ttyLYS_prolific_$1"
-    /usr/local/sbin/bounce-mux
-  fi
-}&
+#!/bin/sh
+chmod a+r /dev/ttyLYS_prolific_"$1"
+if [[ $2 == "remove" ]] ; then
+  logger "The USB device /dev/ttyLYS_prolific_$1 has been disconnected"
+else
+  /usr/local/sbin/bounce-mux
+  logger "This USB device is known as Prolific and will be linked on /dev/ttyLYS_prolific_$1"
+fi
