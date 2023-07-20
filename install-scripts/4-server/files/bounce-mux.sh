502fcc2fc1541cecae64f98e67de9615dfe658c7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-07-20 04:07:30 +0000

    Update bounce-mux.sh

--- install-scripts/4-server/files/bounce-mux.sh
+++ install-scripts/4-server/files/bounce-mux.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-systemctl is-enabled kplex && systemctl restart kplex
-
-sleep 1
-
-systemctl is-enabled signalk && systemctl restart signalk
+/usr/bin/su -c '/usr/bin/systemctl is-enabled signalk && /usr/bin/systemctl restart signalk'
+/usr/bin/su -c '/usr/bin/systemctl is-enabled kplex && /usr/bin/systemctl restart kplex'
