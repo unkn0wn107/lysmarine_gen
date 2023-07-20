d76615a3077fd3867ba7ebea08bdb216fbc01011
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-07-20 04:00:57 +0000

    Update manage_gps.sh

--- install-scripts/4-server/files/manage_gps.sh
+++ install-scripts/4-server/files/manage_gps.sh
@@ -1,24 +1,22 @@
-#!/bin/bash
-{
-  chmod a+r /dev/ttyLYS_gps_"$1"
-  if [[ $2 == "remove" ]] ; then
-    logger "The USB device /dev/ttyLYS_gps_$1 has been disconnected"
-    if [[ $1 == "0" ]] ; then
-      gpsdctl remove /dev/ttyLYS_gps_0
-    else
-      systemctl stop lysgpsd@"$1".service
-    fi
+#!/bin/sh
+chmod a+r /dev/ttyLYS_gps_"$1"
+if [[ $2 == "remove" ]] ; then
+  logger "The USB device /dev/ttyLYS_gps_$1 has been disconnected"
+  if [[ $1 == "0" ]] ; then
+    /usr/bin/su -c '/usr/bin/gpsdctl remove /dev/ttyLYS_gps_0'
   else
-    if [[ $1 == "0" ]] ; then
-      logger "This USB device is known as GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
-      systemctl restart gpsd
-      export GPSD_OPTIONS="-p"
-      gpsdctl add /dev/ttyLYS_gps_0
-    else
-      logger "This USB device is known as GPS and will be connected to gpsd on port 2947$1 /dev/ttyLYS_gps_$1"
-      systemctl restart lysgpsd@"$1".service
-      sleep 1
-      systemctl is-enabled signalk && systemctl restart signalk
-    fi
+    /usr/bin/su -c '/usr/bin/systemctl stop lysgpsd@'"$1"'.service'
   fi
-}&
+else
+  if [[ $1 == "0" ]] ; then
+    /usr/bin/su -c '/usr/bin/systemctl restart gpsd'
+    logger "This USB device is known as GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
+    GPSD_OPTIONS="-p"
+    export GPSD_OPTIONS
+    /usr/bin/su -c '/usr/bin/gpsdctl add /dev/ttyLYS_gps_0'
+  else
+    /usr/bin/su -c '/usr/bin/systemctl restart lysgpsd@'"$1"'.service'
+    /usr/bin/su -c '/usr/bin/systemctl is-enabled signalk && systemctl restart signalk'
+    logger "This USB device is known as GPS and will be connected to gpsd on port 2947$1 /dev/ttyLYS_gps_$1"
+  fi
+fi
