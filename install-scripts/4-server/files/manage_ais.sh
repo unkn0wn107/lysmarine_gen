f9eb41adfc1a7e00747977f88e741053c835b2c7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-07-20 03:53:47 +0000

    Update manage_ais.sh

--- install-scripts/4-server/files/manage_ais.sh
+++ install-scripts/4-server/files/manage_ais.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-{
-  chmod a+r /dev/ttyLYS_ais_"$1"
-  if [[ $2 == "remove" ]] ; then
-    logger "The USB device /dev/ttyLYS_ais_$1 has been disconnected"
-  else
-    logger "This USB device is known as AIS and will be linked on /dev/ttyLYS_ais_$1"
-    /usr/local/sbin/bounce-mux
-  fi
-}&
+#!/bin/sh
+chmod a+r /dev/ttyLYS_ais_"$1"
+if [[ $2 == "remove" ]] ; then
+  logger "The USB device /dev/ttyLYS_ais_$1 has been disconnected"
+else
+  /usr/local/sbin/bounce-mux
+  logger "This USB device is known as AIS and will be linked on /dev/ttyLYS_ais_$1"
+fi
