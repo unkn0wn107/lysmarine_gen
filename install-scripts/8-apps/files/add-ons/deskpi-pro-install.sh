70ee9540c6d261bbfcd020affec05d21a6ecb0a1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-07-21 15:09:59 +0000

    Update deskpi-pro-install.sh
    https://github.com/bareboat-necessities/lysmarine_gen/issues/316

--- install-scripts/8-apps/files/add-ons/deskpi-pro-install.sh
+++ install-scripts/8-apps/files/add-ons/deskpi-pro-install.sh
@@ -8,6 +8,14 @@ cd ~/deskpi/
 
 myArch=$(dpkg --print-architecture)
 
+echo "Note: this software assumes /dev/ttyUSB0 is used for fan control"
+echo "      which might be not the case on your system and it can cause"
+echo "      weird USB errors and resets."
+echo "      Read https://github.com/DeskPi-Team/deskpi/tree/master/drivers"
+echo "      If unsure turn off fan control service:"
+echo " sudo systemctl disable deskpi"
+echo " sudo systemctl stop deskpi"
+
 if [ "arm64" != "$myArch" ] ; then
   chmod +x install.sh
   sudo ./install.sh
