917dc68931e357d7ef805853fb33eff629eee497
mgrouch <mgrushinskiy@gmail.com>
2023-11-14 19:28:02 -0500

    gtk4

--- install-scripts/6-desktop/03-budgie-desktop.sh
+++ install-scripts/6-desktop/03-budgie-desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get install -y -q libatk-adaptor libgtk-3-0 libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 \
+apt-get install -y -q libatk-adaptor libgtk-4-1 libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 \
   libgdk-pixbuf2.0-0 libglib2.0-0  \
   libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 librsvg2-common libx11-6 menulibre \
   gvfs-fuse gvfs-backends gnome-bluetooth gnome-weather geoclue-2-demo ibus
