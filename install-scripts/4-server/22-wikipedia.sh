d1fef6a58f318aa41b37e153acb485ade991a315
mgrouch <mgrushinskiy@gmail.com>
2023-11-15 01:09:33 -0500

    fixing

--- install-scripts/4-server/22-wikipedia.sh
+++ install-scripts/4-server/22-wikipedia.sh
@@ -1,13 +1,3 @@
 #!/bin/bash -e
 
-apt-get install -y liblzma5 libicu72 libzstd1 libxapian30 libcurl3-gnutls aria2
-
-exit 0 # TODO: disabled temporarily
-
-if [ "$LMARCH" == 'arm64' ]; then
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/libzim7_7.2.0.0_arm64.deb
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/libkiwix10_10.1.1.0_arm64.deb
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/kiwix-tools_3.3.0.0_arm64.deb
-  dpkg -i libzim7_*.deb libkiwix10_*.deb kiwix-tools_*.deb
-  rm libzim7_*.deb libkiwix10_*.deb kiwix-tools_*.deb
-fi
+apt-get install -y liblzma5 libicu72 libzstd1 libxapian30 libcurl3-gnutls aria2 libkiwix11 kiwix-tools
