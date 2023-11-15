6ecc23f4613203f7e35a197b743073c41ab259e5
mgrouch <mgrushinskiy@gmail.com>
2023-11-14 21:27:56 -0500

    fixing

--- install-scripts/4-server/11-samba.sh
+++ install-scripts/4-server/11-samba.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-echo "samba-common samba-common/workgroup string WORKGROUP" | sudo debconf-set-selections
-echo "samba-common samba-common/dhcp boolean true" | sudo debconf-set-selections
-echo "samba-common samba-common/do_debconf boolean true" | sudo debconf-set-selections
+echo "samba-common samba-common/workgroup string WORKGROUP" | debconf-set-selections
+echo "samba-common samba-common/dhcp boolean true" | debconf-set-selections
+echo "samba-common samba-common/do_debconf boolean true" | debconf-set-selections
 
 apt-get -y -q install samba samba-common samba-client smbnetfs winbind
 
