f051755e4dd2978145aef4f9312854bc0ec7643f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-11-15 20:05:27 -0500

    Update 18-weather-routing.sh

--- install-scripts/6-desktop/18-weather-routing.sh
+++ install-scripts/6-desktop/18-weather-routing.sh
@@ -3,6 +3,9 @@
 # https://github.com/dakk/gweatherrouting
 
 apt-get -y install libeccodes0 libeccodes-tools libosmgpsmap-1.0-1 gir1.2-osmgpsmap-1.0
+
+exit 0 # disabled temp
+
 git clone -b gshhs2.3.6 https://github.com/dakk/gweatherrouting
 cd gweatherrouting/
 make -j 4 install
