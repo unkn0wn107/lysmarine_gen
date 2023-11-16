07642fdb9147dcc88950dfcc2e7c001a597b28d5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-11-16 14:44:58 -0500

    Update 15-java.sh

--- install-scripts/8-apps/15-java.sh
+++ install-scripts/8-apps/15-java.sh
@@ -10,5 +10,5 @@ fi
 if [ "$LMARCH" == 'arm64' ]; then
   apt-get -y install openjdk-17-jdk openjdk-17-jdk-headless openjdk-17-jre openjdk-17-jre-headless
 
-  apt-get -y remove --purge openjdk-11-jdk openjdk-11-jdk-headless openjdk-11-jre openjdk-11-jre-headless
+  #apt-get -y remove --purge openjdk-11-jdk openjdk-11-jdk-headless openjdk-11-jre openjdk-11-jre-headless
 fi
