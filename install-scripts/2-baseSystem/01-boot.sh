41ca3c28a92d7816c8173ce6bb9610895a6af139
mgrouch <mgrushinskiy@gmail.com>
2023-11-18 15:39:25 -0500

    fixing

--- install-scripts/2-baseSystem/01-boot.sh
+++ install-scripts/2-baseSystem/01-boot.sh
@@ -23,8 +23,7 @@ if [ -f /boot/cmdline.txt ]; then
   sed -i 's#console=serial0,9600 ##' /boot/cmdline.txt
   sed -i 's#console=/dev/serial0,9600 ##' /boot/cmdline.txt
 
-	#TODO: disabled
-	#setterm -cursor on >> /etc/issue
+	setterm -cursor on >> /etc/issue
 	echo 'i2c_dev' | tee -a /etc/modules
 fi
 
