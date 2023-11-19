0e5551980c84c1d94227d7413a6a9814cbe0aad2
mgrouch <mgrushinskiy@gmail.com>
2023-11-18 19:54:18 -0500

    fixing first boot

--- install-scripts/2-baseSystem/04-timeServer.sh
+++ install-scripts/2-baseSystem/04-timeServer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-mkdir -p /var/log/chrony
-
-apt-get install -y -q chrony
-systemctl disable systemd-timesyncd || true
-
-## TimeZone
-ln -fs /usr/share/zoneinfo/Etc/UTC /etc/localtime
-dpkg-reconfigure -f noninteractive tzdata
+#mkdir -p /var/log/chrony
+#
+#apt-get install -y -q chrony
+#systemctl disable systemd-timesyncd || true
+#
+### TimeZone
+#ln -fs /usr/share/zoneinfo/Etc/UTC /etc/localtime
+#dpkg-reconfigure -f noninteractive tzdata
