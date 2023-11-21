b000bee98dbed3314e2f7b7713876c7d19ef7000
mgrouch <mgrushinskiy@gmail.com>
2023-11-21 02:08:47 -0500

    fixing first boot, removed fkms. Seems fixed first boot

--- install-scripts/2-baseSystem/00-users.sh
+++ install-scripts/2-baseSystem/00-users.sh
@@ -22,7 +22,7 @@ fi
 ## Add default user.
 adduser --uid 1000 --home /home/user --quiet --disabled-password -gecos "lysmarine" user
 echo 'user:changeme' | chpasswd
-#echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
 usermod -a -G netdev user
 usermod -a -G adm user
 usermod -a -G tty user
@@ -44,39 +44,35 @@ usermod -a -G bluetooth user
 usermod -a -G games user
 usermod -a -G users user
 
-
+# LIRC
 groupadd -r lirc
 useradd -r -g lirc -d /var/lib/lirc -s /usr/bin/nologin -c "LIRC daemon user" lirc
 usermod -a -G input lirc
 
-### Manage the permissions and privileges.
-#if [[ -d /etc/polkit-1 ]]; then
-#	echo "polkit found, adding rules"
-#	install -d "/etc/polkit-1/localauthority/50-local.d"
-#	install -v "$FILE_FOLDER"/mount.pkla  "/etc/polkit-1/localauthority/50-local.d/"
-#	install -v "$FILE_FOLDER"/all_all_users_to_shutdown_reboot.pkla "/etc/polkit-1/localauthority/50-local.d/"
-#	install -d "/etc/polkit-1/localauthority/10-vendor.d"
-#	install -v "$FILE_FOLDER"/org.freedesktop.NetworkManager.pkla  "/etc/polkit-1/localauthority/10-vendor.d/"
-#fi
-#
-#if [[ -f /etc/sudoers.d/010_pi-nopasswd ]]; then # Remove no-pwd sudo to user pi.
-#	rm /etc/sudoers.d/010_pi-nopasswd
-#fi
-#
-#echo 'PATH="/sbin:/usr/sbin:$PATH"' >> /home/user/.profile # Give user capability to halt and reboot.
-#
-#if [ -f /root/.not_logged_in_yet ]; then # Disable first login script.
-#	rm /root/.not_logged_in_yet
-#fi
+## Manage the permissions and privileges.
+if [[ -d /etc/polkit-1 ]]; then
+	echo "polkit found, adding rules"
+	install -d "/etc/polkit-1/localauthority/50-local.d"
+	install -v "$FILE_FOLDER"/mount.pkla  "/etc/polkit-1/localauthority/50-local.d/"
+	install -v "$FILE_FOLDER"/all_all_users_to_shutdown_reboot.pkla "/etc/polkit-1/localauthority/50-local.d/"
+	install -d "/etc/polkit-1/localauthority/10-vendor.d"
+	install -v "$FILE_FOLDER"/org.freedesktop.NetworkManager.pkla  "/etc/polkit-1/localauthority/10-vendor.d/"
+fi
+
+if [[ -f /etc/sudoers.d/010_pi-nopasswd ]]; then # Remove no-pwd sudo to user pi.
+	rm /etc/sudoers.d/010_pi-nopasswd
+fi
+
+echo 'PATH="/sbin:/usr/sbin:$PATH"' >> /home/user/.profile # Give user capability to halt and reboot.
+
+if [ -f /root/.not_logged_in_yet ]; then # Disable first login script.
+	rm /root/.not_logged_in_yet
+fi
 
 ## Prevent the creation of useless home folders on first boot.
 if [ -f /etc/xdg/user-dirs.defaults ]; then
   sed -i 's/^TEMPLATES=/#&/'   /etc/xdg/user-dirs.defaults
   sed -i 's/^PUBLICSHARE=/#&/' /etc/xdg/user-dirs.defaults
-#  sed -i 's/^DESKTOP=/#&/'     /etc/xdg/user-dirs.defaults
-#  sed -i 's/^MUSIC=/#&/'       /etc/xdg/user-dirs.defaults
-#  sed -i 's/^PICTURES=/#&/'    /etc/xdg/user-dirs.defaults
-#  sed -i 's/^VIDEOS=/#&/'      /etc/xdg/user-dirs.defaults
 fi
 
 ln -s /root /home/root # for openplotter
