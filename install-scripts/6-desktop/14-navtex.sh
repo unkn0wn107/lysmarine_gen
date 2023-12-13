d444c57e080d254c42bc075bc8234c23f92a6c27
mgrouch <mgrushinskiy@gmail.com>
2023-12-13 16:43:19 -0500

    added pc-navtex back

--- install-scripts/6-desktop/14-navtex.sh
+++ install-scripts/6-desktop/14-navtex.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -e
 
-apt-get install -y zlib1g libqt5sql5 libqt5serialport5 libqt5widgets5 libqt5dbus5 libqt5sql5-sqlite qtchooser # TODO: pc-navtex
+apt-get install -y zlib1g libqt5sql5 libqt5serialport5 libqt5widgets5 libqt5dbus5 libqt5sql5-sqlite qtchooser
+
+if [ "$LMARCH" == 'arm64' ]; then
+  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/pc-navtex_1.0.0.1_arm64.deb -O pc-navtex.deb
+fi
+
+dpkg -i pc-navtex.deb && rm -f pc-navtex.deb
