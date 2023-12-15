81aecc3cd1f6da38eb9ee82034e7c7219c5300ed
mgrouch <mgrushinskiy@gmail.com>
2023-12-14 20:04:49 -0500

    customizations

--- install-scripts/6-desktop/01-desktopTheming.sh
+++ install-scripts/6-desktop/01-desktopTheming.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-git clone -q --depth=1 --single-branch --branch Abyss-Desktop-Theme-Icons-and-Folders https://github.com/rtlewis88/rtl88-Themes.git
-cp -rf rtl88-Themes/Abyss-DEEP-Suru-GLOW /usr/share/icons/Abyss-DEEP-Suru-GLOW
-rm -rf rtl88-Themes
+#git clone -q --depth=1 --single-branch --branch Abyss-Desktop-Theme-Icons-and-Folders https://github.com/rtlewis88/rtl88-Themes.git
+#cp -rf rtl88-Themes/Abyss-DEEP-Suru-GLOW /usr/share/icons/Abyss-DEEP-Suru-GLOW
+#rm -rf rtl88-Themes
 
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/"
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/feh/"
