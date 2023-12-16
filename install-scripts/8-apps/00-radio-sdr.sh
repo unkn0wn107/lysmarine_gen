7e99af6a1fb0f59a0678b0e654c67e4e2ca02836
mgrouch <mgrushinskiy@gmail.com>
2023-12-16 14:09:11 -0500

    fixing

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -144,7 +144,7 @@ rm -rf kalibrate-rtl/
 
 # AIS-Catcher https://github.com/jvde-github/AIS-catcher
 apt-get install -y librtlsdr0 libairspy0 libairspyhf1 \
-  libhackrf0 libsoapysdr0.7 libzmq3-dev libcurl4-openssl-dev zlib1g
+  libhackrf0 libsoapysdr0.8 libzmq3-dev libcurl4-openssl-dev zlib1g
 
 wget -q -O - https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/AIS-catcher-20231216-bookworm-arm64.zip > AIS-catcher.zip
 unzip AIS-catcher.zip && rm AIS-catcher.zip
