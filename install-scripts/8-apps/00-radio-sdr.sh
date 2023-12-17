c72ed38b6f5ed3734df4e8e796e9ce3ad4d76648
mgrouch <mgrushinskiy@gmail.com>
2023-12-17 15:35:50 -0500

    fixing

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -14,7 +14,8 @@ install -v -m 0644 "$FILE_FOLDER"/88-nuand-bootloader.rules "/etc/udev/rules.d/"
 install -v -m 0644 "$FILE_FOLDER"/99-direwolf-cmedia.rules "/etc/udev/rules.d/"
 install -v -m 0644 "$FILE_FOLDER"/99-thumbdv.rules "/etc/udev/rules.d/"
 
-apt-get -y -q install multimon-ng       \
+apt-get -y -q --no-install-recommends install \
+  multimon-ng                           \
   cubicsdr                              \
   cutesdr                               \
   fldigi                                \
