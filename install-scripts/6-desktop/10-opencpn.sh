4a9355cdac66caf4805f18bfb08cdfbb94d65e0d
mgrouch <mgrushinskiy@gmail.com>
2023-12-18 01:09:18 -0500

    fixing

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -55,36 +55,37 @@ rm All_polar_files.zip
 
 cd "$BK_DIR"
 
-mkdir tmp-o-bundle-"$LMARCH" || exit 2
-cd tmp-o-bundle-"$LMARCH"
-
-wget -O opencpn-plugins-bundle-"$LMARCH".tar.gz https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/opencpn-plugins-bundle-o_5_8_x-bookworm-2-"$LMARCH".tar.gz
-gzip -cd opencpn-plugins-bundle-"$LMARCH".tar.gz | tar xvf -
-
-mkdir -p /home/user/.local/lib /home/user/.local/bin /home/user/.local/share /home/user/.local/doc /home/user/.local/include
-cp -r -p lib/* /home/user/.local/lib/
-cp -r -p bin/* /home/user/.local/bin/ || true
-cp -r -p share/* /home/user/.local/share/
-cp -r -p doc/* /home/user/.local/doc/ || true
-cp -r -p include/* /home/user/.local/include/
-
-chown -R user:user /home/user/.local
-
-cd ..
-rm -rf tmp-o-bundle-"$LMARCH"
-
-if [ -f /home/user/.local/lib/opencpn/libPolar_pi.so ]; then
-  mv /home/user/.local/lib/opencpn/libPolar_pi.so /usr/lib/opencpn/libpolar_pi.so
-fi
-
-if [ -f /home/user/.local/lib/opencpn/liblogbookkonni_pi.so ]; then
-  rm -f /home/user/.local/lib/opencpn/libLogbookKonni_pi.so
-fi
-
-mv /home/user/.local/share/opencpn/plugins/tactics_pi/data/Tactics.svg /home/user/.local/share/opencpn/plugins/tactics_pi/data/tactics.svg
-mv /home/user/.local/share/opencpn/plugins/tactics_pi/data/Tactics_rollover.svg /home/user/.local/share/opencpn/plugins/tactics_pi/data/tactics_rollover.svg
-mv /home/user/.local/share/opencpn/plugins/tactics_pi/data/Tactics_toggled.svg /home/user/.local/share/opencpn/plugins/tactics_pi/data/tactics_toggled.svg
-#mv /home/user/.local/share/opencpn/plugins/CanadianTides_pi/data/canadiantides_panel_icon.png /home/user/.local/share/opencpn/plugins/CanadianTides_pi/data/CanadianTides_panel_icon.png
+#TODO: temp disabled
+#mkdir tmp-o-bundle-"$LMARCH" || exit 2
+#cd tmp-o-bundle-"$LMARCH"
+#
+#wget -O opencpn-plugins-bundle-"$LMARCH".tar.gz https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/opencpn-plugins-bundle-o_5_8_x-bookworm-2-"$LMARCH".tar.gz
+#gzip -cd opencpn-plugins-bundle-"$LMARCH".tar.gz | tar xvf -
+#
+#mkdir -p /home/user/.local/lib /home/user/.local/bin /home/user/.local/share /home/user/.local/doc /home/user/.local/include
+#cp -r -p lib/* /home/user/.local/lib/
+#cp -r -p bin/* /home/user/.local/bin/ || true
+#cp -r -p share/* /home/user/.local/share/
+#cp -r -p doc/* /home/user/.local/doc/ || true
+#cp -r -p include/* /home/user/.local/include/
+#
+#chown -R user:user /home/user/.local
+#
+#cd ..
+#rm -rf tmp-o-bundle-"$LMARCH"
+#
+#if [ -f /home/user/.local/lib/opencpn/libPolar_pi.so ]; then
+#  mv /home/user/.local/lib/opencpn/libPolar_pi.so /usr/lib/opencpn/libpolar_pi.so
+#fi
+#
+#if [ -f /home/user/.local/lib/opencpn/liblogbookkonni_pi.so ]; then
+#  rm -f /home/user/.local/lib/opencpn/libLogbookKonni_pi.so
+#fi
+#
+#mv /home/user/.local/share/opencpn/plugins/tactics_pi/data/Tactics.svg /home/user/.local/share/opencpn/plugins/tactics_pi/data/tactics.svg
+#mv /home/user/.local/share/opencpn/plugins/tactics_pi/data/Tactics_rollover.svg /home/user/.local/share/opencpn/plugins/tactics_pi/data/tactics_rollover.svg
+#mv /home/user/.local/share/opencpn/plugins/tactics_pi/data/Tactics_toggled.svg /home/user/.local/share/opencpn/plugins/tactics_pi/data/tactics_toggled.svg
+##mv /home/user/.local/share/opencpn/plugins/CanadianTides_pi/data/canadiantides_panel_icon.png /home/user/.local/share/opencpn/plugins/CanadianTides_pi/data/CanadianTides_panel_icon.png
 
 wget --no-check-certificate https://download.tuxfamily.org/xinutop/rastow/rastow-0.4.tgz
 gzip -cd rastow-0.4.tgz | tar xvf -
