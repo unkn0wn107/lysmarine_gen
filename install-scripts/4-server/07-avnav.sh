ec0d509e1ed8da8f02f6bc544caadc7a10b95460
mgrouch <mgrushinskiy@gmail.com>
2023-12-19 12:01:11 -0500

    fixing

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -6,13 +6,16 @@ apt-get -q -y --no-install-recommends install avnav xterm mpg123 xvfb
 
 apt-get -q -y install avnav-history-plugin  avnav-more-nmea-plugin avnav-mapproxy-plugin # TODO: ???  avnav-raspi
 
+#apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
 
 wget -O avnav-ocharts-plugin.deb https://www.free-x.de/debpreview/pool/main/a/avnav-ocharts-plugin/avnav-ocharts-plugin_20231216-raspbian-bookworm_arm64.deb
 wget -O avnav-ocharts.deb https://www.free-x.de/debpreview/pool/main/a/avnav-ocharts/avnav-ocharts_1.0.44.0-1bookworm1_arm64.deb
 dpkg -i avnav-ocharts-plugin.deb avnav-ocharts.deb
 rm -f avnav-ocharts-plugin.deb avnav-ocharts.deb
 
-#apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
+wget -O avnav-sailinstrument-plugin.deb https://www.free-x.de/debpreview/pool/main/a/avnav-sailinstrument-plugin/avnav-sailinstrument-plugin_20230723_all.deb
+dpkg -i avnav-sailinstrument-plugin.deb
+rm -f avnav-sailinstrument-plugin.deb
 
 install -o 0 -g 0 -d /usr/lib/systemd/system/avnav.service.d
 install -o 0 -g 0 -m 0644 "$FILE_FOLDER"/lys-avnav.conf /usr/lib/systemd/system/avnav.service.d/
