31995d362c59ca285967b28796a5c2642b5a0dfa
mgrouch <mgrushinskiy@gmail.com>
2023-12-20 09:41:42 -0500

    if [ "$BBN_KIND" == "LIGHT" ] ; then   exit 0 fi

--- install-scripts/4-server/05-bbn-launcher.sh
+++ install-scripts/4-server/05-bbn-launcher.sh
@@ -15,7 +15,11 @@ install -m 755 -d -o www-data -g www-data "/var/www/bbn-launcher"
 install -m 755 -d -o www-data -g www-data "/var/www/bbn-launcher/img"
 install -m 755 -d -o www-data -g www-data "/var/www/bbn-launcher/js"
 install -m 644 -v -o www-data -g www-data "$FILE_FOLDER"/bbn-launcher/bbn-launcher.js "/var/www/bbn-launcher/"
-install -m 644 -v -o www-data -g www-data "$FILE_FOLDER"/bbn-launcher/constants.js "/var/www/bbn-launcher/"
+if [ "$BBN_KIND" == "LIGHT" ] ; then
+  install -m 644 -v -o www-data -g www-data "$FILE_FOLDER"/bbn-launcher/constants-light.js "/var/www/bbn-launcher/constants.js"
+else
+  install -m 644 -v -o www-data -g www-data "$FILE_FOLDER"/bbn-launcher/constants.js "/var/www/bbn-launcher/"
+fi
 install -m 644 -v -o www-data -g www-data "$FILE_FOLDER"/bbn-launcher/js/swiped-events.js "/var/www/bbn-launcher/js"
 install -m 755 -v -o www-data -g www-data "$FILE_FOLDER"/bbn-launcher/bbn-launcherd "/usr/local/bin/"
 
---
+++ install-scripts/4-server/files/bbn-launcher/constants-light.js
@@ -0,0 +1,30 @@
+module.exports = {
+  commands1 : [
+    {name: 'chart', title: 'Chart', img: 'chart', bg: 'Peru', cmd: 'onlyone', args: ['/usr/bin/opencpn', '-l=error']},
+    {name: 'dash', title: 'Dashboard', img: 'dashboard', bg: 'Olive', cmd: '/opt/kip-dash/kip-dash', args: []},
+    {name: 'pypilot', title: 'PyPilot', img: 'autopilot', bg: 'ForestGreen', cmd: '/opt/Pypilot_webapp/Pypilot_webapp', args: []},
+    {name: 'weather', title: 'Weather', img: 'weather', bg: 'RoyalBlue', cmd: 'onlyone', args: ['XyGrib']},
+    {name: 'winds', title: 'Winds', img: 'race', bg: 'DarkSeaGreen', cmd: 'gtk-launch', args: ['windy']},
+
+    {name: 'signalk', title: 'Signal K', img: 'dashboard', bg: 'ForestGreen', cmd: 'gtk-launch', args: ['signalk']},
+    {name: 'tasks', title: 'Tasks', img: 'tasks', bg: 'Olive',  cmd: 'onlyone', args: ['lxtask']},
+    {name: 'terminal', title: 'Terminal', img: 'terminal', bg: 'RoyalBlue',  cmd: 'onlyone', args: ['lxterminal']},
+    {name: 'web-weather', title: 'Forecast', img: 'weather', bg: 'SeaGreen',  cmd: 'onlyone', args: ['gnome-weather']},
+    {name: 't-storms', title: 'T-Storms', img: 'weather', bg: 'Tan', cmd: 'gtk-launch', args: ['lightningmaps']},
+
+    {name: 'power', title: 'Power', img: 'solar', bg: 'Tan', cmd: 'gtk-launch', args: ['victron']},
+    {name: 'drone', title: 'Drones', img: 'drone', bg: 'SaddleBrown', cmd: 'onlyone', args: ['apmplanner2']},
+    {name: 'commands', title: 'Commands', img: 'travel', bg: 'Tan',  cmd: '/usr/local/bin/bbn-commands', args: []},
+    {name: 'www', title: 'WWW', img: 'internet', bg: 'SteelBlue', cmd: 'onlyone', args: ['gnome-www-browser']},
+    {name: 'files', title: 'Files', img: 'folder', bg: 'ForestGreen', cmd: 'onlyone', args: ['thunar']},
+  ],
+
+  commands2 : [
+  ],
+
+  commands3 : [
+  ],
+
+  sites : [
+  ]
+}
