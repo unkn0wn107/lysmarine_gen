690b0ca3f34c2b0db999c5b114d09ee48380f9d2
mgrouch <mgrushinskiy@gmail.com>
2023-12-20 13:59:22 -0500

    if [ "$BBN_KIND" == "LITE" ] ; then   exit 0 fi

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -69,6 +69,7 @@ rm -rf /usr/share/doc/tcllib/html/
 rm -rf /usr/share/doc/openjdk*/test*/*
 rm -rf /usr/share/doc/python3*/HISTORY.*
 rm -rf /usr/share/doc/python3*/NEWS.*
+rm -rf /var/lib/apt/lists/*
 rm -f /2
 find /usr/share/doc -name changelog\*.gz -exec rm -f {} \;
 find /usr/share/doc -name NEWS\*.gz -exec rm -f {} \;
