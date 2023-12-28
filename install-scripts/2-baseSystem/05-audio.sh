c4ecb8ff5ee59ab43e28744b09984ef52234d45b
mgrouch <mgrushinskiy@gmail.com>
2023-12-28 16:10:06 -0500

    fixing

--- install-scripts/2-baseSystem/05-audio.sh
+++ install-scripts/2-baseSystem/05-audio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get -y -q install pulseaudio
+apt-get -y -q install pulseaudio pulseaudio-module-bluetooth
 usermod -a -G audio user
 
 echo "set-default-source alsa_output.platform-bcm2835_audio.analog-mono.2.monitor" >> /etc/pulse/default.pa
