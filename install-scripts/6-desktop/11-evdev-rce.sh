72610ea6538bb5ee832c4aaf0aa1ac70e4cbef66
mgrouch <mgrushinskiy@gmail.com>
2024-01-04 16:52:55 -0500

    https://github.com/bareboat-necessities/lysmarine_gen/issues/139

--- install-scripts/6-desktop/11-evdev-rce.sh
+++ install-scripts/6-desktop/11-evdev-rce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ "$LMARCH" == 'arm64' ]; then
-  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/evdev-rce_1.0.0.1_arm64.deb -O evdev-rce.deb
+  wget https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/evdev-rce_1.0.0.4_arm64.deb -O evdev-rce.deb
 fi
 
 dpkg -i evdev-rce.deb && rm -f evdev-rce.deb
