14581d162d7168371123319ef19e7959f2fb0b2b
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-01-12 21:02:05 -0500

    winlink pat

--- cross-build-release/raspbian.sh
+++ cross-build-release/raspbian.sh
@@ -75,9 +75,9 @@ EOF
 
   # Renaming the OS and moving it to the release folder.
   if [ "$BBN_KIND" == "LITE" ] ; then
-    BBN_IMG=lysmarine-bbn-lite-bookworm_"${LYSMARINE_VER}"-${thisArch}-${cpuArch}.img
+    BBN_IMG=lysmarine-bbn-beta-lite-bookworm_"${LYSMARINE_VER}"-${thisArch}-${cpuArch}.img
   else
-    BBN_IMG=test-bbn-full-bookworm_"${LYSMARINE_VER}"-${thisArch}-${cpuArch}.img
+    BBN_IMG=lysmarine-bbn-beta-full-bookworm_"${LYSMARINE_VER}"-${thisArch}-${cpuArch}.img
   fi
   cp -v ./work/$thisArch/"$imageName" ./release/$thisArch/"$BBN_IMG"
 
