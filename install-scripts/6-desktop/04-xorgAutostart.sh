9653d70d0ed02a29431bd9e1b581b4fcadea8073
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-01-27 20:58:11 -0500

    Adding LightDM for
    https://github.com/BuddiesOfBudgie/budgie-desktop/issues/508

--- install-scripts/6-desktop/04-xorgAutostart.sh
+++ install-scripts/6-desktop/04-xorgAutostart.sh
@@ -2,7 +2,7 @@
 
 install -v -m644 "$FILE_FOLDER"/startx.service "/etc/systemd/system/startx.service"
 
-systemctl enable startx.service
+systemctl disable startx.service
 systemctl set-default graphical.target
 
 
