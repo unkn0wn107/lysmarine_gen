e7e820c4c2670e0d31066640bb2ed9c8b04edb3f
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-02-06 11:59:31 -0500

    upd

--- install-scripts/8-apps/05-video-player.sh
+++ install-scripts/8-apps/05-video-player.sh
@@ -8,5 +8,6 @@ apt-get clean
 
 apt-get -y -q install vlc webcamoid vokoscreen # totem
 
+install -o 1000 -g 1000 -d /home/user/.config/Webcamoid
 install -o 1000 -g 1000 -v "$FILE_FOLDER"/Webcamoid.conf /home/user/.config/Webcamoid/
 
