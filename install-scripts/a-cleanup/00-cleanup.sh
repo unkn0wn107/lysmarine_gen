ac87e48ab5a9d8cf0fa49a187fe98270d7dc3760
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-02-07 16:24:05 -0500

    upd

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -61,7 +61,8 @@ fi
 
 apt-get clean
 
-apt-get remove -y --purge greybird-gtk-theme murrine-themes rpd-icons userconf-pi gdb libsdl2-dev libicu-dev libnorm-dev
+apt-get remove -y --purge greybird-gtk-theme murrine-themes rpd-icons userconf-pi gdb libsdl2-dev libicu-dev \
+  libnorm-dev libavcodec-dev libfftw3-dev
 
 if [ "$BBN_KIND" == "LITE" ] ; then
   apt-get remove -y --purge system-config-printer gnome-power-manager
