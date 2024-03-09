a172d79611e9bbadf1330b036f040196ea64ae50
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-03-09 11:21:59 -0500

    upd

--- install-scripts/4-server/20-media-lib.sh
+++ install-scripts/4-server/20-media-lib.sh
@@ -4,6 +4,13 @@ if [ "$BBN_KIND" == "LITE" ] ; then
   exit 0
 fi
 
-apt-get install -y jellyfin
+#apt-get install -y jellyfin
+
+wget -q -O - https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-server_10.8.13-1_arm64.deb > jellyfin-server.deb
+wget -q -O - https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-web_10.8.13-1_all.deb > jellyfin-web.deb
+wget -q -O - https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin_10.8.13-1_all.deb < jellyfin.deb
+
+dpkg -i jellyfin*.deb
+rm -rf jellyfin*.deb
 
 systemctl disable jellyfin
