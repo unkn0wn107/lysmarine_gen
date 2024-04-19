cc60a2f01a8ef07fec198bce5f61cc26d3f89f9a
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-04-18 23:46:48 -0400

    upd

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -10,8 +10,8 @@ apt-get -y -q install geographiclib-tools libexiv2-27 libnlopt0 libnlopt-cxx0 \
  libqt6charts6 libqt6multimediawidgets6 libqt6webenginewidgets6 libqt6serialport6 # libqt6script6 stellarium stellarium-data
 
 xargs -n 1 -P 2 wget -q << EOF
-http://ppa.launchpad.net/stellarium/stellarium-releases/ubuntu/pool/main/s/stellarium/stellarium_24.1.0-upstream1.0~ubuntu22.04.1_arm64.deb
-http://ppa.launchpad.net/stellarium/stellarium-releases/ubuntu/pool/main/s/stellarium/stellarium-data_24.1.0-upstream1.0~ubuntu22.04.1_all.deb
+https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/stellarium_24.1.0-upstream1.0~ubuntu22.04.1_arm64.deb
+https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/stellarium-data_24.1.0-upstream1.0.ubuntu22.04.1_all.deb
 EOF
 
 dpkg-deb -xv stellarium_*.deb /
