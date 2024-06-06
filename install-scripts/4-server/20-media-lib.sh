20fca86c1fa94477d78e6a455b0605a981a076e6
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-06-06 18:33:52 -0400

    upd

--- install-scripts/4-server/20-media-lib.sh
+++ install-scripts/4-server/20-media-lib.sh
@@ -7,9 +7,9 @@ fi
 #apt-get install -y jellyfin
 
 xargs -n 1 -P 4 wget -q << EOF
-https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-server_10.9.4%2Bdeb12_arm64.deb
-https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin_10.9.4%2Bdeb12_all.deb
-https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-web_10.9.4%2Bdeb11_all.deb
+https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-server_10.9.6%2Bdeb12_arm64.deb
+https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin_10.9.6%2Bdeb12_all.deb
+https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-web_10.9.6%2Bdeb11_all.deb
 https://repo.jellyfin.org/releases/server/debian/versions/jellyfin-ffmpeg/6.0.1-3/jellyfin-ffmpeg6_6.0.1-3-bookworm_arm64.deb
 EOF
 
