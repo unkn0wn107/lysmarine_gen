8f49ec6be7caa3fb558489a79330d3e62fcd681a
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-08 13:11:55 -0400

    boatinstrument

--- install-scripts/4-server/20-media-lib.sh
+++ install-scripts/4-server/20-media-lib.sh
@@ -7,6 +7,8 @@ fi
 #apt-get install -y jellyfin
 
 apt-get clean
+npm cache clean --force || true
+rm -rf ~/.local/share/pnpm
 
 xargs -n 1 -P 4 wget -q << EOF
 https://repo.jellyfin.org/files/server/debian/latest-stable/arm64/jellyfin-server_10.9.9%2Bdeb12_arm64.deb
