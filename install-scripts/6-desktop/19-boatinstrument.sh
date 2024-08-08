91bb67ff30e85f875fd360b759d857aff0d4d9e1
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-08 12:20:28 -0400

    boatinstrument

--- install-scripts/6-desktop/19-boatinstrument.sh
+++ install-scripts/6-desktop/19-boatinstrument.sh
@@ -9,7 +9,7 @@ BK_DIR="$(pwd)"
 
 cd /home/user
 
-wget -O boatinstrument.tgz https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/boatinstrument-0.0.2.1-linux-aarch64.tgz
+wget -O boatinstrument.tgz https://github.com/bareboat-necessities/lysmarine_gen/releases/download/vTest/boatinstrument-0.0.4.1-linux-aarch64.tgz
 gzip -cd < boatinstrument.tgz | tar xvf -
 chown -R user:user ./boatinstrument/
 rm -f boatinstrument.tgz
