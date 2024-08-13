1455f6ad7491e6b918febcc9e5a9b2564f7992bb
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-08-13 13:44:44 -0400

    upd

--- install-scripts/0-repositories/00-addRepo.sh
+++ install-scripts/0-repositories/00-addRepo.sh
@@ -64,7 +64,7 @@ curl -1sLf https://raw.githubusercontent.com/bareboat-necessities/lysmarine_gen/
 #curl -1sLf https://open-mind.space/repo/open-mind.space.gpg.key | apt-key add -     # AvNav
 curl -1sLf https://raw.githubusercontent.com/bareboat-necessities/lysmarine_gen/master/public-keys/flightaware/gpg.flightaware.key | apt-key add -
 
-wget -O /etc/apt/trusted.gpg.d/abcd567a-key.gpg https://abcd567a.github.io/rpi/KEY2.gpg # PiAware
+wget -O /etc/apt/trusted.gpg.d/abcd567a-key.gpg https://github.com/abcd567a/abcd567a.github.io/raw/master/debian12/KEY2.gpg # PiAware
 wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | gpg --dearmor | tee /usr/share/keyrings/box86-debs-archive-keyring.gpg # Box86
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor > /usr/share/keyrings/grafana.gpg
 
