ce181d6b7d45b207bb5e92d831b7db89837b5634
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-09-02 09:37:01 -0400

    upd

--- install-scripts/8-apps/19-wikipedia.sh
+++ install-scripts/8-apps/19-wikipedia.sh
@@ -4,4 +4,15 @@ if [ "$BBN_KIND" == "LITE" ] ; then
   exit 0
 fi
 
+npm cache clean --force
+
+# remove python pip cache
+rm -rf ~/.cache/pip
+
+# remove all cache
+rm -rf ~/.cache
+rm -rf ~/.config
+rm -rf ~/.npm
+rm -rf ~/.wget*
+
 apt-get -y install libqt5webenginecore5 libqt5webenginewidgets5 zim-tools kiwix
