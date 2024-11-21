49d031ddac67ce1e1b7b6130796b837a3fb0ac55
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-11-21 18:43:38 -0500

    upd

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -179,5 +179,6 @@ rm -f /usr/share/applications/yad-icon-browser.desktop
 sed -i 's/#RebootWatchdogSec=10min/RebootWatchdogSec=75s/' /etc/systemd/system.conf
 
 # Fill free space with zeros
-cat /dev/zero > /zer0s || true
-rm -f /zer0s
+# Commented out as pishrink does it now
+#cat /dev/zero > /zer0s || true
+#rm -f /zer0s
