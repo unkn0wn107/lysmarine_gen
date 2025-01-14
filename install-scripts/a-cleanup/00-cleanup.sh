6ad05d4e4d68f87b709b4c650c1e444c2cbc968b
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-01-14 00:33:32 -0500

    upd

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -8,6 +8,10 @@ rm -rf $(find /var/log/ -type f)
 rm -f /opt/vc/src/hello_pi/hello_video/test.h264
 rm -f /usr/lib/python3/dist-packages/scipy/io/tests/data/*.wav
 rm -rf /usr/share/doc/python3-pyglet/examples/
+rm -rf /usr/share/doc/openjdk*/test*/*
+rm -rf /usr/share/doc/python3*/HISTORY.*
+rm -rf /usr/share/doc/python3*/NEWS.*
+rm -rf /usr/share/backgrounds/budgie/*
 
 apt-get clean
 apt-get -y update
