291214408fbad5dbea988c6cd181901189dc7118
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-02-07 07:42:04 -0500

    upd

--- cross-build-release/lib.sh
+++ cross-build-release/lib.sh
@@ -95,7 +95,7 @@ inflateImage() {
 
     if [ "$BBN_KIND" == "LITE" ] ; then
       log "truncate image to 8.5G"
-      truncate -s "8704M" "$imageLocationInflated"
+      truncate -s "8832M" "$imageLocationInflated"
     else
       log "truncate image to 14.5G"
       truncate -s "14848M" "$imageLocationInflated"
