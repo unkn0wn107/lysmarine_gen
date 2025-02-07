3ee1d047d3a278ce189592f83bf99c8eb69a0b90
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-02-07 07:26:57 -0500

    upd

---
+++ install-scripts/4-server/24-ocearo-ui.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+if [ "$BBN_KIND" == "FULL" ] ; then
+  exit 0
+fi
+
+pushd /home/signalk/.signalk/node_modules
+  su signalk --shell=/bin/bash -c " \
+  git clone --depth=1 https://github.com/laborima/ocearo-ui; \
+  cd ocearo-ui; \
+  npm install next react react-dom; \
+  export MAKEFLAGS='-j 8'; \
+  export NODE_ENV=production; \
+  npm run build; \
+  cp -r ./out/* public/; rm -rf ./out; \
+  rm -rf ./.git; cd .."
+popd
+
+bash -c 'cat << EOF > /usr/local/share/applications/ocearo-ui.desktop
+[Desktop Entry]
+Type=Application
+Name=Ocearo-UI
+GenericName=Ocearo-UI
+Comment=Ocearo-UI 3D
+Exec=gnome-www-browser http://localhost:3000/ocearo-ui
+# Read: https://github.com/laborima/ocearo-ui
+Terminal=false
+Icon=gnome-globe
+Categories=Navigation
+EOF'
