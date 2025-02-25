e7a3f02f77b0228430f343fca3b2acbc742dfcb1
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-02-25 11:16:42 -0500

    upd

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -15,6 +15,8 @@ rm -rf /usr/share/doc/openjdk*/test*/*
 rm -rf /usr/share/doc/python3*/HISTORY.*
 rm -rf /usr/share/doc/python3*/NEWS.*
 rm -rf /usr/share/backgrounds/budgie/*
+rm -rf /opt/arduino-*/examples
+rm -rf /opt/arduino-*/libraries/*/examples
 
 find /usr/share/doc -name changelog\*.gz -exec rm -f {} \;
 find /usr/share/doc -name NEWS\*.gz -exec rm -f {} \;
