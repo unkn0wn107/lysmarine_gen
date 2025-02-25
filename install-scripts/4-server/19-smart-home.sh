ccf6bb4bf6fdfaa72979d9446b409dbeb4b28f77
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2025-02-25 12:46:20 -0500

    upd

--- install-scripts/4-server/19-smart-home.sh
+++ install-scripts/4-server/19-smart-home.sh
@@ -31,7 +31,7 @@ su homeassistant --shell=/bin/bash -c "
   python3.11 -m venv . ;
   source bin/activate;
   python3.11 -m pip install wheel;
-  pip3.11 install homeassistant sqlalchemy fnvhash setuptools pyotp PyQRCode;
+  pip3.11 install homeassistant sqlalchemy fnvhash setuptools pyotp PyQRCode josepy==1.15.0 certifi==2024.12.14;
   mkdir -p /home/homeassistant/.homeassistant;
   rm -rf /home/homeassistant/.cache"
 
@@ -83,7 +83,6 @@ su homeassistant --shell=/bin/bash -c "
   source bin/activate;
   python3.11 -m pip install wheel;
   pip3.11 install esphome tornado esptool;
-  pip3.11 install josepy==1.15.0 certifi==2024.12.14
   rm -rf /home/homeassistant/.cache"
 
 bash -c 'cat << EOF > /etc/systemd/system/esphome@homeassistant.service
